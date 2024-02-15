1424bfb61a565c46693360a7bcb5766a0fde8e49
Muthu Ganesh G <mutgan7@gmail.com>
2024-02-15 09:04:57 +0530

    Time: 176 ms (35.39%) | Memory: 19.6 MB (72.03%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -8,7 +8,6 @@ class Solution(object):
         for idx,num in enumerate(nums):
             prefix[idx] = (prefix[idx-1] if idx - 1 >= 0 else 1) *num
         suffix = 1
-        print(prefix)
         for pos in range(len(nums)-1,-1,-1):
             num = nums[pos]
             nums[pos] = (prefix[pos-1] if pos - 1 >= 0 else 1)*suffix
