ddebdb99f8e45b36d38fdcff24fe0e85b406fe28
Muthu Ganesh G <mutgan7@gmail.com>
2024-03-04 10:16:37 +0530

    Time: 37 ms (18.52%) | Memory: 11.8 MB (70.37%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def bagOfTokensScore(self, tokens, power):
+        """
+        :type tokens: List[int]
+        :type power: int
+        :rtype: int
+        """
+        tokens = sorted(tokens)
+        left = 0
+        right = len(tokens) - 1
+        score = 0
+        max_score = 0
+        while left <= right:
+            tl = tokens[left]
+            tr = tokens[right]
+            if power - tl >= 0:
+                power = power - tl
+                score+=1
+                max_score = max(score,max_score)
+                left+=1
+            elif score >= 1:
+                power = power + tr
+                score-=1
+                right-=1
+            else:
+                left+=1
+        return max_score
+
+        
